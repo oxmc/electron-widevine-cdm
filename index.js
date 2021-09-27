821240ce6225e68ffae809272aeb0f2fe5107883
oxmc <67136658+oxmc@users.noreply.github.com>
2021-09-27 12:47:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-exports.inject = (app) => {
+exports.load = (app) => {
 	app.commandLine.appendSwitch('widevine-cdm-path', __dirname + '/plugin/');
 	app.commandLine.appendSwitch('widevine-cdm-version', '4.10.1503.4');
 }
